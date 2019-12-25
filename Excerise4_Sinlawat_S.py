f17cf73b90ba82d7f927f7a1d0d2fd1c43881b0a
Sinlawat <Sinlawats58@gmail.com>
2019-12-25 18:50:06 +0700

    New Excercise

---
+++ Excerise4_Sinlawat_S.py
@@ -0,0 +1,15 @@
+Name = "Sinlawat Siwapronwesarut"
+Year = "Sophomore"
+semeter = 1
+FE = 81
+GB = 75
+ICS = 92
+CP = 50
+print("Name ",Name)
+print("Year", Year , "Semeter", semeter ,)
+print("               Your Scores             ")
+print("---------------------------------------")
+print("Foundation English              = ", FE )
+print("Genaral Business                = ", GB )
+print("Introduction to Computer System = ", ICS)
+print("Computer Programming            = ", CP )

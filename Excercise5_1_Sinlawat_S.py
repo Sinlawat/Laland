ed9cf2386d2d576aa37f5a178856ad2669394d42
Sinlawat <Sinlawats58@gmail.com>
2019-12-30 19:06:53 +0700

    add new excercise

---
+++ Excercise5_1_Sinlawat_S.py
@@ -0,0 +1,10 @@
+x = int(input("input your first number = "))
+y = int(input("input your second number = "))
+A = x + y
+B = x - y
+C = x * y
+D = int(x / y)
+print(x,"+",y ," = " , A)
+print(x,"-",y ," = " , B)
+print(x,"*",y ," = " , C)
+print(x,"/",y ," = " , D)
